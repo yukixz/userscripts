c32e15780a57f407727e4f2d758838d7d5d75a71
Dazzy Ding <h@dazzyd.org>
2020-06-18 10:54:21 +0800

    ff14angler-cleaner: Add more words

--- ff14angler-cleaner/main.user.js
+++ ff14angler-cleaner/main.user.js
@@ -2,7 +2,7 @@
 // @name         饥饿的猫 - 过滤垃圾评论
 // @namespace    https://dazzyd.org/
 // @author       Dazzy Ding
-// @version      0.1
+// @version      0.2
 // @downloadURL  https://raw.githubusercontent.com/yukixz/userscripts/master/ff14angler-cleaner/main.user.js
 // @match        https://*.ff14angler.com/*
 // @grant        none
@@ -14,10 +14,11 @@
 
     const RUBBISH_WORDS = [
         "滚", "妈", "求", "死", "nm", // 单字地图炮
+        "！！",
         "爸爸", "超度", "存在",
-        "蝗虫", "垃圾", "没嘴",
-        "全脱", "脱钩",
-        "小心", "许愿", "咬钩", "杂种", "崽种",
+        "蝗虫", "垃圾", "没嘴", "你吗", "你嘴",
+        "傻逼", "沙雕", "全脱", "脱钩",
+        "小心", "许愿", "咬钩", "杂种", "崽种", "长嘴",
     ];
 
     document.querySelector("div.comment_list").addEventListener('DOMNodeInserted', (e) => {
@@ -30,7 +31,7 @@
             const text = node.textContent.toLowerCase()
             for (const word of RUBBISH_WORDS) {
                 if (text.includes(word)) {
-                    console.info(`匹配关键字：${word}，删除垃圾评论：${text}`)
+                    console.info(`删除垃圾评论：${word}|${text}`)
                     node.remove()
                     break
                 }
