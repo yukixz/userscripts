4c13965e094eda7d7f34c2c45f1fdf6d6ee9b2fb
Dazzy Ding <h@dazzyd.org>
2020-07-05 19:42:35 +0800

    ff14angler-cleaner: Add keyword "你马"

--- ff14angler-cleaner/main.user.js
+++ ff14angler-cleaner/main.user.js
@@ -2,7 +2,7 @@
 // @name         饥饿的猫 - 过滤垃圾评论
 // @namespace    https://dazzyd.org/
 // @author       Dazzy Ding
-// @version      0.3
+// @version      0.4
 // @downloadURL  https://raw.githubusercontent.com/yukixz/userscripts/master/ff14angler-cleaner/main.user.js
 // @match        https://*.ff14angler.com/*
 // @grant        none
@@ -15,7 +15,7 @@
     const RUBBISH_WORDS = [
         "滚", "妈", "求", "死", "nm", // 单字地图炮
         "爸爸", "超度", "存在",
-        "蝗虫", "垃圾", "没嘴", "你吗", "你嘴",
+        "蝗虫", "垃圾", "没嘴", "你马", "你吗", "你嘴",
         "傻逼", "沙雕", "全脱",
         "小心", "许愿", "咬钩", "杂种", "崽种", "长嘴",
     ];
