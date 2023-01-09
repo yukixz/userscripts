dcefa20e65090115dcd43e91f5ba2aa433571958
Dazzy Ding <h@dazzyd.org>
2023-01-09 11:06:26 +0800

    idleinfinity: update filter

--- idleinfinity/filter.user.js
+++ idleinfinity/filter.user.js
@@ -1,19 +1,17 @@
 // ==UserScript==
-// @name         Idle Infinity - Item parts
+// @name         Idle Infinity - Item Filter
 // @namespace    http://dazzyd.org/
 // @version      0.1.0
 // @description  Idle Infinity
 // @author       Dazzy Ding
 // @grant        none
-// @match        https://www.idleinfinity.cn/*
+// @match        https://www.idleinfinity.cn/Config/Query?*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=idleinfinity.cn
 // ==/UserScript==
 
 
 // Config
 const config = {
-    initial_ms: 2000,
-    interval_ms: 1200,
     rules: [
         ["全部", "武器", ["+ 骑士光环技能", 2]],
         ["全部", "防具", ["+ 骑士光环技能", 2]],
@@ -30,7 +28,7 @@ const config = {
         ["全部", "武器", ["+ 战士作战技能", 1]],
         ["全部", "防具", ["+ 战士作战技能", 1]],
         ["全部", "饰品", ["+ 战士作战技能", 1]],
-        ["全部", "武器", ["+ 游侠辅助技能", 1]],
+        ["全部", "武器", ["+ 游侠辅助技能", 3]],
         ["全部", "防具", ["+ 游侠辅助技能", 1]],
         ["全部", "饰品", ["+ 游侠辅助技能", 1]],
         ["全部", "武器", ["+ 牧师暗影技能", 1]],
@@ -45,7 +43,7 @@ const config = {
         ["全部", "武器", ["+ 贤者自然技能", 1]],
         ["全部", "防具", ["+ 贤者自然技能", 1]],
         ["全部", "饰品", ["+ 贤者自然技能", 1]],
-        ["全部", "武器", ["+ 猎手陷阱技能", 1]],
+        ["全部", "武器", ["+ 猎手陷阱技能", 3]],
         ["全部", "防具", ["+ 猎手陷阱技能", 1]],
         ["全部", "饰品", ["+ 猎手陷阱技能", 1]],
 
@@ -75,8 +73,9 @@ const config = {
         ["全部", "全部", ["单项元素抗性之和%", 100]],
         ["全部", "饰品", ["+% 元素抗性", 10]],
         ["全部", "护符", ["+ 召唤技能最大召唤数量", 1]],
-        ["魔法", "项链", ["物品等级", 60]],
-        ["魔法", "护符", ["物品等级", 60]],
+        // ["魔法", "项链", ["物品等级", 60]],
+        // ["魔法", "护符", ["物品等级", 60]],
+        ["全部", "秘境"],
         ["套装", "全部"],
         ["传奇", "全部"],
 
@@ -88,27 +87,20 @@ const config = {
 }
 
 
-function add(rule) {
-    const btn_add_rule = document.querySelector("a[data-target='#modalConfig']")
-    const btn_add_cond = document.querySelector("a[data-target='#modalConfig']")
-}
-
-
-function run() {
-    let current = {}
+function init() {
+    let current_rules = {}
     for (const row of document.querySelectorAll('tbody tr')) {
         let parts = []
         const tds = row.querySelectorAll('td')
-        const index = tds[0].innerText
         parts.push(tds[1].innerText)
         parts.push(tds[2].innerText)
         for (const div of tds[3].querySelectorAll('div.col-sm-6')) {
             parts.push(div.innerText)
         }
-        current[parts.join('|')] = index
+        current_rules[parts.join('|')] = row
     }
 
-    let require = {}
+    let require_rules = {}
     for (const row of config.rules) {
         let parts = []
         parts.push(row[0])
@@ -116,19 +108,72 @@ function run() {
         for (const [title, value] of row.slice(2)) {
             parts.push(`【${title}】 >= ${value}`)
         }
-        require[parts.join('|')] = row
+        require_rules[parts.join('|')] = row
     }
 
-    for (const [key, rule] of Object.entries(require)) {
-        if (current[key] == null) {
-            console.log(key)
+    // 高亮不在配置中的多余规则
+    for (const [key, dom] of Object.entries(current_rules)) {
+        if (require_rules[key] == null) {
+            console.log(`多余规则：${key}`)
+            dom.style.backgroundColor = "#900"
+        }
+    }
+
+    // 缺少规则
+    for (const [key, rule] of Object.entries(require_rules)) {
+        if (current_rules[key] == null) {
+            console.log(`缺少规则：${key}`)
+            confirm(`是否添加规则：${rule}`, () => {
+                setInterval(add_rule, 500, rule)
+            })
         }
     }
 }
 
 
+let add_step = 0
 
-setTimeout(() => {
-    run()
-    // setInterval(run, config.interval_ms)
-}, config.initial_ms)
+function add_rule(rule) {
+    function select_value(dom, value) {
+        for (const [index, option] of Object.entries(dom.options)) {
+            if (option.text === value) {
+                dom.selectedIndex = index
+                return
+            }
+        }
+        console.error(`无法在DOM${dom}中找到选项${value}`)
+    }
+
+    const btn_open_modal = document.querySelector("a[data-target='#modalConfig']")
+    const div_modal = document.querySelector("#modalConfig")
+    const btn_add_cond = div_modal.querySelector("button.config-magic-add")
+    const btn_submit = div_modal.querySelector("button.config-apply")
+
+    if (add_step === 0) {
+        btn_open_modal.click()
+        select_value(div_modal.querySelector("select#power"), rule[0])
+        select_value(div_modal.querySelector("select#type"), rule[1])
+        for (const _ of rule.slice(2)) {
+            btn_add_cond.click()
+        }
+        add_step = 1
+        return
+    }
+    if (add_step === 1) {
+        const div_conds = div_modal.querySelectorAll("div.condition")
+        for (const [index, [title, value]] of rule.slice(2).entries()) {
+            const div = div_conds[index]
+            select_value(div.querySelector("select.prefix"), title)
+            div.querySelector("input.min").value = value
+        }
+        add_step = 2
+        return
+    }
+    if (add_step === 2) {
+        btn_submit.click()
+    }
+}
+
+
+
+setTimeout(init, 0)
