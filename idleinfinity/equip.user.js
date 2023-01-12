f8bb9743607058e1c9b168b0222ec4fa13f70a8a
Dazzy Ding <h@dazzyd.org>
2023-01-12 17:13:00 +0800

    idleinfinity: equip.js v0.4.1

--- idleinfinity/equip.user.js
+++ idleinfinity/equip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Idle Infinity - Equipment
 // @namespace    http://dazzyd.org/
-// @version      0.4.0
+// @version      0.4.1
 // @description  Idle Infinity
 // @author       Dazzy Ding
 // @license      MIT
@@ -22,6 +22,38 @@ const store = {
     localStorage.setItem(`dd_ui_equip`, JSON.stringify(this))
   },
 }
+const tipRules = {
+  "职业技能": [/\+(\d+) .*?(.{2})技能/g, "", "skill"],
+  "召唤数量": [/\+(\d+) (.{1,6})最大召唤数量/g, "", "magic"],
+  "增强伤害(ed)": [/\+(\d+)\% 增强伤害/g, "ed", "physical"],
+  "攻击速度(ias)": [/攻击速度提升 (\d+)\%/g, "ias", "physical"],
+  "施法速度(fcr)": [/施法速度提升 (\d+)\%/g, "fcr", "magic"],
+  "魔法装备(mf)": [/\+(\d+)\% 更佳的机会取得魔法装备/g, "mf", "state"],
+  "额外金币(gf)": [/\+(\d+)\% 额外金币从怪物身上取得/g, "gf", "lightning"],
+  "元素抗性(res)": [/元素抗性 \+(\d+)\%/g, "res", "skill"],
+  "抗火(f)": [/抗火 \+(\d+)/g, "f", "fire"],
+  "抗寒(c)": [/抗寒 \+(\d+)/g, "c", "cold"],
+  "抗闪电(l)": [/抗闪电 \+(\d+)/g, "l", "lightning"],
+  "抗毒(p)": [/抗毒 \+(\d+)/g, "p", "poison"],
+  "凹槽": [/凹槽(\(0\/\d+\))/g, "", ""],
+  "双手武器": [/双手伤害：/g, "2H", ""],
+  "需要等级": [/需要等级：(\d+)/g, "rlv", ""],
+}
+const defaultTips = [
+  "职业技能",
+  "召唤数量",
+  "增强伤害(ed)",
+  "攻击速度(ias)",
+  "施法速度(fcr)",
+  "魔法装备(mf)",
+  "额外金币(gf)",
+  "元素抗性(res)",
+  "抗火(f)",
+  "抗寒(c)",
+  "抗闪电(l)",
+  "抗毒(p)",
+  "凹槽",
+]
 
 
 function parseContent(id, node) {
@@ -72,23 +104,9 @@ function addTips(id, equipNode) {
 
   // 通过regexp提取信息
   const content = store[id]
-  for (const [regex, suffix, className] of [
-    [/\+(\d+) .*?(.{2})技能/g, "", "skill"],
-    [/\+(\d+) (.{1,6})最大召唤数量/g, "", "magic"],
-    [/\+(\d+)\% 增强伤害/g, "ed", "physical"],
-    [/攻击速度提升 (\d+)\%/g, "ias", "physical"],
-    [/施法速度提升 (\d+)\%/g, "fcr", "magic"],
-    [/\+(\d+)\% 更佳的机会取得魔法装备/g, "mf", "state"],
-    [/\+(\d+)\% 额外金币从怪物身上取得/g, "gf", "lightning"],
-    [/元素抗性 \+(\d+)\%/g, "res", "skill"],
-    [/抗火 \+(\d+)/g, "f", "fire"],
-    [/抗寒 \+(\d+)/g, "c", "cold"],
-    [/抗闪电 \+(\d+)/g, "l", "lightning"],
-    [/抗毒 \+(\d+)/g, "p", "poison"],
-    [/凹槽(\(0\/\d+\))/g, "", ""],
-    // [/双手伤害：/g, "2H", ""],
-    // [/需要等级：(\d+)/g, "rlv", ""],
-  ]) {
+  const tipNameList = store.tips || defaultTips
+  for (const name of tipNameList) {
+    const [regex, suffix, className] = tipRules[name]
     const matches = content.matchAll(regex)
     for (const match of matches) {
       const value = match.slice(1).join('')
@@ -135,8 +153,27 @@ function init() {
   for (const node of document.querySelectorAll(".equip-content-container .equip-content")) {
     observer.observe(node, { childList: true })
   }
+
   // 增加CSS，鼠标移上去隐藏标记，避免内容过长换行
   GM_addStyle(`.equip-container > p:hover > .tips { display: none; }`)
+}
+
+function initUI() {
+  /*
+  <div class="btn-group open">
+    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+      TIPS <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu">
+      <li><a class="physical" >全部</a></li>
+      <li><a class="base" ><input type="checkbox">近战武器</a></li>
+      <li><a class="base" ><input type="checkbox">远程武器</a></li>
+      <li><a class="base" ><input type="checkbox">元素武器</a></li>
+      <li><a class="base" ><input type="checkbox">防具</a></li>
+      <li><a class="base" ><input type="checkbox">饰品</a></li>
+    </ul>
+  </div>
+  */
 }
 
 
