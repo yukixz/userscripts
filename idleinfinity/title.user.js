62ee97878a74571c0090524c92b3c0135dd0f332
Dazzy Ding <h@dazzyd.org>
2023-01-16 12:41:36 +0800

    idleinfinity: title.js v0.3.1

--- idleinfinity/title.user.js
+++ idleinfinity/title.user.js
@@ -10,8 +10,64 @@
 // ==/UserScript==
 
 
+const rules = {
+  "/Home/Index": "主页",
+
+  "/Character/Detail": "角色",
+  "/Skill/Config": "技能",
+  "/Equipment/Query": "装备",
+  "/Equipment/Material": "材料/符文/宝石",
+  "/Battle/BattleField": "战场",
+  "/Character/Group": "团队",
+
+  "/Map/Detail": "地图",
+  "/Map/Dungeon": "秘境",
+  "/Battle/InDungeon": "战斗",
+
+  "/Config/Query": "物品过滤",
+  "/Character/RankingList": "赛季排行",
+  "/Character/AllChar": "角色等级排行",
+  "/Character/AllCharMCU": "符文掉落排行",
+
+  "/Help/Content?url=Base": "帮助-基础知识",
+  "/Help/Content?url=CharType": "帮助-职业介绍",
+  "/Help/Content?url=Monsters": "帮助-怪物介绍",
+  "/Help/Content?url=Equip": "帮助-物品装备",
+  "/Help/Content?url=BaseEquip": "帮助-普通物品",
+  "/Help/Content?url=Prefix": "帮助-前缀",
+  "/Help/Content?url=Suffix": "帮助-后缀",
+  "/Help/Content?url=SpecialAffix": "帮助-固定词缀",
+  "/Help/Content?url=Set": "帮助-套装",
+  "/Help/Content?url=Sacred": "帮助-圣衣",
+  "/Help/Content?url=Unique": "帮助-传奇物品",
+  "/Help/Content?url=Artifact": "帮助-神器",
+
+  "/Guide/Query": "攻略列表",
+  "/Guide/Detail": "攻略详情",
+}
+
+function getCharName() {
+  const firstNavItem = document.querySelector("#navbar > ul > li")
+  if (firstNavItem.classList.contains("dropdown")) {
+    return `[${firstNavItem.firstElementChild.text}]`
+  }
+}
+
+function getPathName() {
+  for (const key of [location.pathname, `${location.pathname}${location.search}`]) {
+    if (rules[key] != null) {
+      return rules[key]
+    }
+  }
+  return location.pathname
+}
+
 function update_title() {
-  let title = location.pathname
+  let parts = [
+    getCharName(),
+    getPathName(),
+  ]
+
   if (location.pathname === "/Map/Dungeon") {
     const explored = document.querySelector('.panel .explore').textContent
     const monster = document.querySelector('.panel .monster-left').textContent
@@ -31,10 +87,8 @@ function update_title() {
       setTimeout(update_title, 1000)
     }
   }
-  if (location.pathname.startsWith("/Help/")) {
-    title = `${location.pathname}${location.search}`
-  }
-  document.title = title
+
+  document.title = parts.filter(x => x != null).join(' ')
 }
 
 setTimeout(update_title, 0)
